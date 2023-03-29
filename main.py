57aa49328abb3d20d4547942c78623f643b64c6e
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-03-29 16:11:15 -0400

    Added necessary imports to WebScraper class

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from web_scraper.scraper import WebScraper
-import requests
-from bs4 import BeautifulSoup
 
 scraper = WebScraper('www.google.com')
 scraper.print_url()
--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 class WebScraper:
     def __init__(self, url):
         self.url = url
