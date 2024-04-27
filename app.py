0524116260bdc18e247c7bdf033c79773c33e62a
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-04-26 22:20:43 -0400

    created web scraper class

--- app.py
+++
@@ -1,6 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-res = requests.get('https://www.crummy.com/software/BeautifulSoup/bs4/doc/')
-soup = BeautifulSoup(res.content, 'html.parser')
-print(soup.prettify())
---
+++ web_scraper.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+class WebScraper:
+    def __init__(self, url):
+        self.url = url
+        res = requests.get(self.url)
+        self.soup = BeautifulSoup(res.content, 'html.parser')
+
+    def get_url(self):
+        return self.url
+    
+    def get_full_html(self):
+        return self.soup.prettify()
+    
+    def get_by_tag(self, tag, is_find_many = False):
+        result = None
+        if (is_find_many):
+            result = self.soup.find_all(tag)
+        else:
+            result = self.soup.find(tag).get_text()
+        return result
+    
+    def get_by_class(self, tag, class_name, is_find_many = False):
+        result = None
+        if (is_find_many):
+            result = self.soup.find_all(tag, class_ = class_name)
+        else:
+            result = self.soup.find(tag, class_ = class_name).get_text()
+        return result
+    
+    def get_by_id(self, tag, id):
+        return self.soup.find(tag, id = id).get_text()
