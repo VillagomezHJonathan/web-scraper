5d4fd8dc7dba5d0ffd70f95989693ad47056f646
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-04-28 13:59:15 -0400

    finished get table method

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ scrape = WebScraper('https://www.acefitness.org/resources/everyone/exercise-libr
 
 dict = {
   'name': '.exercise-card__title',
-  'body_part': '.exercise-info__term--body-part dd',
-  'equipment': '.exercise-info__term--equipment dd',
-  'difficulty': '.exercise-info__term--difficulty .exercise-info__meter'
+  'body_part': '.exercise-info__term--body-part dd'
 }
 print(scrape.get_table(dict))
